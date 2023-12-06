d28f97436f3dff1009ec2a41d0447e868c4324a1
Philip Kendall <philip@shadowmagic.org.uk>
2023-12-06 10:50:26 +0000

    Learning more about Rust.

--- 05/05-2.rs
+++ 05/05-2.rs
@@ -1,4 +1,4 @@
-use std::cmp::{min, max};
+use std::cmp::min;
 use std::env;
 use std::iter::zip;
 use std::fs::read_to_string;
@@ -7,22 +7,22 @@ fn read_lines(filename: &str) -> Vec<String> {
     read_to_string(filename).unwrap().lines().map(String::from).collect()
 }
 
-fn parse_seeds(line: &String) -> Vec<(i64, i64)> {
+fn parse_seeds(line: &str) -> Vec<(i64, i64)> {
     let numbers: Vec<_> = line.split(": ").nth(1).unwrap().split(' ').map(|s| s.parse().unwrap()).collect();
     let starts = numbers.iter().step_by(2).copied();
     let lengths = numbers.iter().skip(1).step_by(2).copied();
-    return zip(starts, lengths).map(|(s, l)| (s, s + l)).collect()
+    zip(starts, lengths).map(|(s, l)| (s, s + l)).collect()
 }
 
 fn parse_mapping_line(line: &String) -> (i64, i64, i64) {
     let values: Vec<_> = line.split(" ").map(|s| s.parse().unwrap()).collect();
-    return (values[1], values[1] + values[2], values[0] - values[1])    
+    (values[1], values[1] + values[2], values[0] - values[1])
 }
 
 fn parse_map(chunk: &[String]) -> Vec<(i64, i64, i64)> {
     let mut mapping_lines: Vec<_> = chunk.iter().skip(1).map(parse_mapping_line).collect();
     mapping_lines.sort();
-    return mapping_lines
+    mapping_lines
 }
 
 fn apply(data: (i64, i64), map: &Vec<(i64, i64, i64)>) -> Vec<(i64, i64)> {
@@ -31,27 +31,23 @@ fn apply(data: (i64, i64), map: &Vec<(i64, i64, i64)>) -> Vec<(i64, i64)> {
     let (data_start, data_end) = data;
     let mut current_pos = data_start;
 
-    for mapping_line in map {
-        let (source_start, source_end, diff) = *mapping_line;
-
-        if source_start >= data_end {
+    for (source_start, source_end, diff) in map {
+        if *source_start >= data_end {
             break
         }
 
-        if source_end < data_start {
+        if *source_end < data_start {
             continue
         }
 
-        if current_pos < source_start {
-            new_data.push((current_pos, source_start))
+        if current_pos < *source_start {
+            new_data.push((current_pos, *source_start));
+            current_pos = *source_start;
         }
 
-        let start_point = max(current_pos, source_start);
-        let end_point = min(source_end, data_end);
+        let end_point = min(*source_end, data_end);
 
-        if source_start < end_point {
-            new_data.push((start_point + diff, end_point + diff))
-        }
+        new_data.push((current_pos + *diff, end_point + *diff));
 
         current_pos = end_point
     }
@@ -60,7 +56,7 @@ fn apply(data: (i64, i64), map: &Vec<(i64, i64, i64)>) -> Vec<(i64, i64)> {
         new_data.push((current_pos, data_end))
     }
 
-    return new_data
+    new_data
 }
 
 fn main() {
