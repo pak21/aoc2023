1cc5614b5768167b74a5037985e345a76a355897
Philip Kendall <philip@shadowmagic.org.uk>
2023-12-16 07:22:23 +0000

    Tidy.

--- 16/16.py
+++ 16/16.py
@@ -1,70 +1,56 @@
 #!/usr/bin/env python3
 
+import collections
 import sys
 
 puzzle = {}
 
 with open(sys.argv[1]) as f:
-    for y, row in enumerate(f):
-        for x, c in enumerate(row):
-            puzzle[(x, y)] = c
+    puzzle = [l.rstrip() for l in f]
 
-max_x = x
-max_y = y+1
+max_x = len(puzzle[0])
+max_y = len(puzzle)
+
+def next_beam(old_x, old_y, direction):
+    match direction:
+        case 0: return (old_x+1, old_y)
+        case 1: return (old_x, old_y-1)
+        case 2: return (old_x-1, old_y)
+        case 3: return (old_x, old_y+1)
 
 def part1(initial):
     seen = set()
-    todo = [initial]
+    todo = collections.deque([initial])
 
     while todo:
-        (old_x, old_y), direction = todo.pop(0)
-        if ((old_x, old_y), direction) in seen:
+        (old_x, old_y), direction = todo.popleft()
+
+        if ((old_x, old_y), direction) in seen or old_x < 0 or old_x >= max_x or old_y < 0 or old_y >= max_y:
             continue
 
         seen.add(((old_x, old_y), direction))
 
-        new_beams = []
-        cell = puzzle.get((old_x, old_y))
+        new_dirs = []
+        match (puzzle[old_y][old_x], direction):
+            case ('.', _): new_dirs = [direction]
 
-        match (cell, direction):
-            case ('.', 0): new_beams.append(((old_x + 1, old_y), 0))
-            case ('.', 1): new_beams.append(((old_x, old_y - 1), 1))
-            case ('.', 2): new_beams.append(((old_x - 1, old_y), 2))
-            case ('.', 3): new_beams.append(((old_x, old_y + 1), 3))
+            case ('/', 0): new_dirs = [1]
+            case ('/', 1): new_dirs = [0]
+            case ('/', 2): new_dirs = [3]
+            case ('/', 3): new_dirs = [2]
 
-            case ('/', 0): new_beams.append(((old_x, old_y - 1), 1))
-            case ('/', 1): new_beams.append(((old_x + 1, old_y), 0))
-            case ('/', 2): new_beams.append(((old_x, old_y + 1), 3))
-            case ('/', 3): new_beams.append(((old_x - 1, old_y), 2))
+            case ('\\', 0): new_dirs = [3]
+            case ('\\', 1): new_dirs = [2]
+            case ('\\', 2): new_dirs = [1]
+            case ('\\', 3): new_dirs = [0]
 
-            case ('\\', 0): new_beams.append(((old_x, old_y + 1), 3))
-            case ('\\', 1): new_beams.append(((old_x - 1, old_y), 2))
-            case ('\\', 2): new_beams.append(((old_x, old_y - 1), 1))
-            case ('\\', 3): new_beams.append(((old_x + 1, old_y), 0))
+            case ('|', _) if direction % 2 == 0: new_dirs = [1, 3]
+            case ('|', _) if direction % 2 == 1: new_dirs = [direction]
 
-            case ('|', 0):
-                new_beams.append(((old_x, old_y - 1), 1))
-                new_beams.append(((old_x, old_y + 1), 3))
-            case ('|', 1): new_beams.append(((old_x, old_y - 1), 1))
-            case ('|', 2):
-                new_beams.append(((old_x, old_y - 1), 1))
-                new_beams.append(((old_x, old_y + 1), 3))
-            case ('|', 3): new_beams.append(((old_x, old_y + 1), 3))
+            case ('-', _) if direction % 2 == 0: new_dirs = [direction]
+            case ('-', _) if direction % 2 == 1: new_dirs = [0, 2]
 
-            case ('-', 0): new_beams.append(((old_x + 1, old_y), 0))
-            case ('-', 1):
-                new_beams.append(((old_x + 1, old_y), 0))
-                new_beams.append(((old_x - 1, old_y), 2))
-            case ('-', 2): new_beams.append(((old_x - 1, old_y), 2))
-            case ('-', 3):
-                new_beams.append(((old_x + 1, old_y), 0))
-                new_beams.append(((old_x - 1, old_y), 2))
-
-            case _: raise Exception(cell, direction)
-
-        for (new_x, new_y), new_dir in new_beams:
-            if new_x >= 0 and new_x < max_x and new_y >= 0 and new_y < max_y:
-                todo.append(((new_x, new_y), new_dir))
+        todo += [(next_beam(old_x, old_y, nd), nd) for nd in new_dirs]
 
     return len(set([s[0] for s in seen]))
 
