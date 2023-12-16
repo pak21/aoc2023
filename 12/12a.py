677e127edb9e952f822d619f03387a82d33f43e9
Philip Kendall <philip@shadowmagic.org.uk>
2023-12-16 07:22:54 +0000

    Very minor tweak.

--- 12/12a.py
+++ 12/12a.py
@@ -31,16 +31,14 @@ def recurse_hash(current_run, springs, target):
 def recurse(current_run, springs, target):
     if springs:
         match springs[0]:
-            case '.': r = recurse_dot(current_run, springs, target)
-            case '#': r = recurse_hash(current_run, springs, target)
-            case '?': r = recurse_dot(current_run, springs, target) + recurse_hash(current_run, springs, target)
+            case '.': return recurse_dot(current_run, springs, target)
+            case '#': return recurse_hash(current_run, springs, target)
+            case '?': return recurse_dot(current_run, springs, target) + recurse_hash(current_run, springs, target)
     else:
         if current_run is None:
-            r = 1 if not target else 0
+            return 1 if not target else 0
         else:
-            r = 1 if len(target) == 1 and current_run == target[0] else 0
-
-    return r
+            return 1 if len(target) == 1 and current_run == target[0] else 0
 
 def main():
     repeats = int(sys.argv[2])
