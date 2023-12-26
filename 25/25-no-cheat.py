5485de70185c279d217699c32e8e079f04e090f6
Philip Kendall <philip@shadowmagic.org.uk>
2023-12-26 10:24:30 +0000

    Tidy.

--- 25/25-no-cheat.py
+++ 25/25-no-cheat.py
@@ -9,17 +9,6 @@ def parse_line(l):
     a, bs = l.split(': ')
     return a, set(bs.split(' '))
 
-random.seed(42)
-
-with open(sys.argv[1]) as f:
-    wires = [parse_line(l.rstrip()) for l in f]
-
-links = collections.defaultdict(set)
-for src, dests in wires:
-    links[src] |= dests
-    for dest in dests:
-        links[dest].add(src)
-
 def find_shortest_path(links, start, end):
     todo = [(start, [])]
     seen = set()
@@ -42,6 +31,33 @@ def find_shortest_path(links, start, end):
 
     raise Exception(f'No path found from {start} to {end}?')
 
+def find_connected_set(links, to_break):
+    todo = [list(links)[0]]
+
+    seen = set()
+    while todo:
+        pos = todo.pop(0)
+
+        if pos in seen:
+            continue
+
+        seen.add(pos)
+
+        for next_pos in links[pos]:
+            if (pos, next_pos) not in to_break and (next_pos, pos) not in to_break:
+                todo.append(next_pos)
+
+    return len(seen)
+
+with open(sys.argv[1]) as f:
+    wires = [parse_line(l.rstrip()) for l in f]
+
+links = collections.defaultdict(set)
+for src, dests in wires:
+    links[src] |= dests
+    for dest in dests:
+        links[dest].add(src)
+
 nodes = list(links.keys())
 
 traversed = collections.defaultdict(int)
@@ -55,31 +71,11 @@ while True:
         key = (a, b) if a < b else (b, a)
         traversed[key] += 1
 
-    to_break = sorted(traversed.items(), key=operator.itemgetter(1))[-3:]
+    to_break = set([pair for pair, _ in sorted(traversed.items(), key=operator.itemgetter(1))[-3:]])
 
-    for (a, b), _ in to_break:
-        links[a].remove(b)
-        links[b].remove(a)
+    connected_count = find_connected_set(links, to_break)
 
-    todo = [list(links)[0]]
-
-    seen = set()
-    while todo:
-        pos = todo.pop(0)
-
-        if pos in seen:
-            continue
-
-        seen.add(pos)
-
-        for next_pos in links[pos]:
-            todo.append(next_pos)
-
-    if len(seen) != len(links):
+    if connected_count != len(links):
         print(f'Found a partition after {i} iterations: {to_break}')
-        print(len(seen) * (len(links) - len(seen)))
+        print(connected_count * (len(links) - connected_count))
         break
-
-    for (a, b), _ in to_break:
-        links[a].add(b)
-        links[b].add(a)
