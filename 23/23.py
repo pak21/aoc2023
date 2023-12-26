f292922b1b0ea94223e30b32e29a070f7a002468
Philip Kendall <philip@shadowmagic.org.uk>
2023-12-26 10:28:57 +0000

    Tidy.

--- 23/23.py
+++ 23/23.py
@@ -11,24 +11,25 @@ def can_move(pos, trails):
 
     return trails[y][x] != '#'
     
-def next_positions(pos, trails, part2):
+def next_positions_part1(pos, trails):
     x, y = pos
-    if part2:
-        return [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1) ]
-    else:
-        match trails[y][x]:
-            case '.': return [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1) ]
-            case '>': return [(x + 1, y)]
-            case 'v': return [(x, y + 1)]
+    match trails[y][x]:
+        case '.': return [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1) ]
+        case '>': return [(x + 1, y)]
+        case 'v': return [(x, y + 1)]
 
-def walk(trails, start, poi, part2):
+def next_positions_part2(pos, trails):
+    x, y = pos
+    return [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1) ]
+
+def walk(trails, start, poi, next_pos_fn):
     todo = [(start, set())]
 
     edges = {}
     while todo:
         pos, seen = todo.pop(0)
 
-        for next_pos in next_positions(pos, trails, part2):
+        for next_pos in next_pos_fn(pos, trails):
             if can_move(next_pos, trails) and next_pos not in seen:
                 if next_pos in poi:
                     edges[next_pos] = len(seen) + 1
@@ -57,22 +58,21 @@ def walk_graph(graph, start, end):
 
     longest = 0
     while todo:
-        negative_moves, pos, seen = heapq.heappop(todo)
-        moves = -negative_moves
+        moves, pos, seen = heapq.heappop(todo)
 
         for next_pos, moves_to_next in graph[pos].items():
             if next_pos not in seen:
-                next_moves = moves + moves_to_next
+                next_moves = moves - moves_to_next
                 if next_pos == end:
-                    if next_moves > longest:
+                    if next_moves < longest:
                         longest = next_moves
                 else:
-                    heapq.heappush(todo, (-next_moves, next_pos, seen | {pos}))
+                    heapq.heappush(todo, (next_moves, next_pos, seen | {pos}))
 
-    return longest
+    return -longest
 
-def do_part(trails, start, end, poi, part2):
-    graph = {start: walk(trails, start, poi, part2) for start in poi}
+def do_part(trails, start, end, poi, next_pos_fn):
+    graph = {start: walk(trails, start, poi, next_pos_fn) for start in poi}
     return walk_graph(graph, start, end)
 
 def main():
@@ -84,8 +84,8 @@ def main():
 
     poi = [start, end] + list(find_junctions(trails))
 
-    for part2 in [False, True]:
-        print(do_part(trails, start, end, poi, part2))
+    for next_pos_fn in [next_positions_part1, next_positions_part2]:
+        print(do_part(trails, start, end, poi, next_pos_fn))
 
 if __name__ == '__main__':
     main()
